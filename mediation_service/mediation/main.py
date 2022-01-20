ac1521a77d63a676643318abbe7d3c7578a580ce
Andre Silva <silva.andre.daniel@gmail.com>
2022-01-20 11:59:59 +0000

    AMB-980 allergyintollerance endpoint created

--- mediation_service/mediation/main.py
+++ mediation_service/mediation/main.py
@@ -11,5 +11,10 @@ def status():
     return Response(status_code=HTTP_200_OK)
 
 
+@app.get("/allergyintollerance")
+def status():
+    return Response(status_code=HTTP_200_OK)
+
+
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=9000)
