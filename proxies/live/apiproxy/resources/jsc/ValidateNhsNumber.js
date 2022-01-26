0ef3d9262292ebec475ba9a6eaa523b5348c597a
Valentinolucas <valentinolucasfantini@gmail.com>
2022-01-26 15:38:54 +0000

    AMB-978 rename vriables and improve error response

--- proxies/live/apiproxy/resources/jsc/ValidateNhsNumber.js
+++ proxies/live/apiproxy/resources/jsc/ValidateNhsNumber.js
@@ -1,16 +1,21 @@
-valid_nhs_number = false
-var id_token_nhs_number = context.getVariable('jwt.DecodeJWT.DecodeIdToken.claim.nhs_number')
-var request_pathsuffix = context.getVariable('request.queryparam.patient');
+var valid_nhs_number = false
+var client_nhs_number = context.getVariable('jwt.DecodeJWT.DecodeIdToken.claim.nhs_number')
+var patient_url = context.getVariable('request.queryparam.patient');
 
-function parse_url(url) {
+function extract_nhs_number(url) {
+    /*
+      Gets the nhs number out from a string
+      url: str
+      return: str
+    */
     var pattern = RegExp("[0-9]{10}");
     var matches =  url.match(pattern);
     return matches
 } 
 
-var request_path_nhs_number = parse_url(request_pathsuffix);
+var request_nhs_number = extract_nhs_number(patient_url);
 
-if (request_path_nhs_number == id_token_nhs_number){
+if (request_nhs_number == client_nhs_number){
     valid_nhs_number = true
 }
 
