ea97b5a1b8795320a73865500aeea1a67c597fa6
Valentinolucas <valentinolucasfantini@gmail.com>
2022-01-27 10:43:43 +0000

    AMB-978 Add integration tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,16 +6,53 @@ from assertpy import assert_that
 class TestAllergyIntolerance:
     @pytest.fixture()
     def url(self) -> str:
-        return (
-            "https://int.api.service.nhs.uk/gp-connect-access-record/AllergyIntolerance"
-        )
+        return "https://int.api.service.nhs.uk/gp-connect-access-record/AllergyIntolerance"
 
     def test_happy_path(self, apigee_token, url):
         # Given
         token = apigee_token
         expected_status_code = 200
         # When
-        response = requests.get(url=url, headers={"Authorization": f"Bearer {token}"})
+        response = requests.get(
+            url=url,
+            headers={"Authorization": f"Bearer {token}"},
+            params={"patient": "https://fhir.nhs.uk/Id/9661034524"},
+        )
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+
+    def test_nhs_number_not_matching(self, apigee_token, url):
+        # Given
+        token = apigee_token
+        expected_status_code = 403
+        # When
+        response = requests.get(
+            url=url,
+            headers={"Authorization": f"Bearer {token}"},
+            params={"patient": "https://fhir.nhs.uk/Id/0000000000"},
+        )
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+
+    @pytest.mark.parametrize(
+        "patient",
+        [
+            "https://fhir.nhs.uk/Id/9661034524/9912003070",
+            "https://fhir.nhs.uk/Id/9912003",
+            "https://example.com/9661034524",
+            "",
+        ],
+    )
+    def test_invalid_patient_query_parameter(self, apigee_token, url, patient):
+        # Given
+        token = apigee_token
+        expected_status_code = 400
+        # When
+        response = requests.get(
+            url=url,
+            headers={"Authorization": f"Bearer {token}"},
+            params={"patient": f"{patient}"},
+        )
         # Then
         assert_that(expected_status_code).is_equal_to(response.status_code)
 
