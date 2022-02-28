240fb52d27cf348224b23231cf3acd6511d7768f
Andre Silva <silva.andre.daniel@gmail.com>
2022-02-28 15:33:50 +0000

    fix test to pass on pipeline

--- mediation_service/mediation/main.py
+++ mediation_service/mediation/main.py
@@ -1,6 +1,8 @@
 import json
 import os
 import re
+from filter_bundle import BundleFilter
+from fhirclient.models.allergyintolerance import AllergyIntolerance
 
 import uvicorn
 from fastapi import FastAPI, Depends, HTTPException, Request
@@ -146,14 +148,14 @@ def allergy_intolerance(
         values
     )  # returned as a json str
 
-    # bundle_filterer = BundleFilter(AllergyIntolerance)
-    # filtered_bundle_json = bundle_filterer.filter_for_resource(allergy_bundle.text)
+    bundle_filterer = BundleFilter(AllergyIntolerance)
+    filtered_bundle_json = bundle_filterer.filter_for_resource(allergy_bundle.text)
     # converted_bundle = _fhir_convert_client.convert(filtered_bundle_json, access_token)
 
     response_for_test_while_using_orange_test = {
         "to_ASID": to_ASID,
         "GPConnect_URL": GPConnect_URL,
-        "response": allergy_bundle.status_code,
+        "response": filtered_bundle_json,
     }
 
     return Response(
