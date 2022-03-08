266e275775052dcd7f6c8c4912f313a887161e2c
hirra farooq <hirra.farooq1@nhs.net>
2022-03-08 16:59:29 +0000

    uncomment tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -59,46 +59,46 @@ class TestAllergyIntolerance:
             response_entry_resourceType_allergy
         )
 
-    # @pytest.mark.mediation
-    # @pytest.mark.debug
-    # def test_nhs_number_not_matching(self, access_token, url):
-    #     # Given
-    #     expected_status_code = 403
-    #     # When
-    #     response = requests.get(
-    #         url=url,
-    #         headers={"Authorization": f"Bearer {access_token}"},
-    #         params={"patient": "https://fhir.nhs.uk/Id/0000000000"},
-    #     )
-    #     # Then
-    #     assert_that(expected_status_code).is_equal_to(response.status_code)
+    @pytest.mark.mediation
+    @pytest.mark.debug
+    def test_nhs_number_not_matching(self, access_token, url):
+        # Given
+        expected_status_code = 403
+        # When
+        response = requests.get(
+            url=url,
+            headers={"Authorization": f"Bearer {access_token}"},
+            params={"patient": "https://fhir.nhs.uk/Id/0000000000"},
+        )
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
 
-    # @pytest.mark.parametrize(
-    #     "patient",
-    #     [
-    #         "https://fhir.nhs.uk/Id/9661034524/9912003070",
-    #         "https://fhir.nhs.uk/Id/9912003",
-    #         "https://example.com/9661034524",
-    #         "",
-    #     ],
-    # )
-    # def test_invalid_patient_query_parameter(self, access_token, url, patient):
-    #     # Given
-    #     expected_status_code = 400
-    #     # When
-    #     response = requests.get(
-    #         url=url,
-    #         headers={"Authorization": f"Bearer {access_token}"},
-    #         params={"patient": f"{patient}"},
-    #     )
-    #     # Then
-    #     assert_that(expected_status_code).is_equal_to(response.status_code)
+    @pytest.mark.parametrize(
+        "patient",
+        [
+            "https://fhir.nhs.uk/Id/9661034524/9912003070",
+            "https://fhir.nhs.uk/Id/9912003",
+            "https://example.com/9661034524",
+            "",
+        ],
+    )
+    def test_invalid_patient_query_parameter(self, access_token, url, patient):
+        # Given
+        expected_status_code = 400
+        # When
+        response = requests.get(
+            url=url,
+            headers={"Authorization": f"Bearer {access_token}"},
+            params={"patient": f"{patient}"},
+        )
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
 
-    # def test_invalid_token(self, url):
-    #     # Given
-    #     token = "invalid"
-    #     expected_status_code = 401
-    #     # When
-    #     response = requests.get(url=url, headers={"Authorization": f"Bearer {token}"})
-    #     # Then
-    #     assert_that(expected_status_code).is_equal_to(response.status_code)
+    def test_invalid_token(self, url):
+        # Given
+        token = "invalid"
+        expected_status_code = 401
+        # When
+        response = requests.get(url=url, headers={"Authorization": f"Bearer {token}"})
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
